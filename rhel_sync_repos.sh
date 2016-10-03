9f3b22d841d643178cd643efe7a3063ec9bfe573
Kurach Alexander <sundelor@gmail.com>
2016-10-03 12:18:45 +0300

    Update rhel_sync_repos.sh
    remake

--- rhel_sync_repos.sh
+++ rhel_sync_repos.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
+yum makecache fast
 repocount=0
-while read line; do
-	reposync -p /var/www/html/repo/ -r $line
-	repocount=$((repocount+1))
-done < repos.name
+while read line
+  do
+    reposync --gpgcheck -l --repoid=$line --download_path=/home/repo/ --downloadcomps --download-metadata
+#    reposync -p /var/www/html/repo/ -r $line
+    repocount=$((repocount+1))
+    createrepo -v /var/www/html/repo/$line
+  done < repos.name
+
 echo "Sync $repocount repository"
